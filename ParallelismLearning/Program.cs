eb4af504234735452d6daa34afa0d2321c371587
riley-converse <riley.converse@yahoo.com>
2023-12-28 00:05:41 -0500

    Initial commit

---
+++ ParallelismLearning/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ParallelismLearning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.For(0,10, i =>
+            {
+                Console.WriteLine($"Task {i} runnign on thread {Task.CurrentId}");
+            });
+
+
+            Parallel.Invoke(
+                () => DoTask("Task 1"),
+                () => DoTask("Task 2"),
+                () => DoTask("Task 3")
+             );
+        }
+
+        static void DoTask(string name)
+        {
+            Console.WriteLine($"{name} running on thread {Task.CurrentId}");
+        }
+    }
+}

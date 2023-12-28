622548adb5ab7faeb80f983eb112b5b12c5539c5
Riley Converse <riley.converse@yahoo.com>
2023-12-28 14:14:27 -0500

    Implemented pattern finding logic
    Used Tasks to speed up pattern searching along with implementing cancellation tokens. Used basic chars instead of advanced patterns. For prototyping purposes only, will need to be implemented differently with the advanced pattern matching.

---
+++ ParallelismLearning/PatternHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelismLearning
+{
+    internal class PatternHandler
+    {
+        private readonly char[] _patterns;
+
+        private int _indexer;
+
+        public int Length => _patterns.Length;
+
+        public PatternHandler(params char[] patterns)
+        {
+            _patterns = patterns;
+            _indexer = 0;
+        }
+
+        public (bool, char) GetPattern()
+        {
+            if (_indexer < _patterns.Length)
+            {
+                char ch = _patterns[_indexer];
+                _indexer++;
+                return (true,ch);
+            }
+            
+            return (false,default);
+        }
+
+        public void ResetIndexer()
+        {
+            _indexer = 0;
+        }
+
+    }
+}
--- ParallelismLearning/Program.cs
+++ ParallelismLearning/Program.cs
@@ -1,25 +1,82 @@
-﻿namespace ParallelismLearning
+﻿using System.Diagnostics;
+
+namespace ParallelismLearning
 {
     internal class Program
     {
+        public delegate void TestDelegate(char ch);
+
+        public delegate void TestDelegate2();
+        
+
         static void Main(string[] args)
         {
-            Parallel.For(0,10, i =>
+            /*Parallel.For(0,10, i =>
             {
-                Console.WriteLine($"Task {i} runnign on thread {Task.CurrentId}");
-            });
+                Console.WriteLine($"Task {i} running on thread {Task.CurrentId}");
+            });*/
 
+            
+            // potential patterns
+            /*Parallel.Invoke(
+                () => DoTask('c'),
+                () => DoTask('b'),
+                () => DoTask('z')
+                );*/
 
-            Parallel.Invoke(
-                () => DoTask("Task 1"),
-                () => DoTask("Task 2"),
-                () => DoTask("Task 3")
-             );
+            PatternHandler pattern = new PatternHandler('i','c','z');
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+
+            List<Task> tasks = new List<Task>();
+
+            TaskFactory factory = new TaskFactory(token);
+            Console.WriteLine(pattern.Length);
+            Stopwatch global = new Stopwatch();
+            global.Start();
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                tasks.Add(factory.StartNew(() => DoTask(pattern.GetPattern().Item2,cts, global),token));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+            global.Stop();
+            Console.WriteLine("Pattern Searched completed in [GLOBAL] "+global.ElapsedMilliseconds);
         }
 
-        static void DoTask(string name)
+
+        static void DoTask(char ch, CancellationTokenSource cts, Stopwatch global)
         {
-            Console.WriteLine($"{name} running on thread {Task.CurrentId}");
+            string input =
+                "Hello world! This is test text figure out which task finds out the first pattern using parallelism z.";
+            int index = 0;
+
+            Console.WriteLine($"[Task {Task.CurrentId}]:\tI've been called to duty. Starting timer.");
+
+            Stopwatch sw = Stopwatch.StartNew();
+            foreach (char c in input)
+            {
+                if (cts.IsCancellationRequested)
+                {
+                    Console.WriteLine($"[Task {Task.CurrentId}]:\tI can't run, my token is cancelled");
+                    sw.Stop();
+                    Console.WriteLine($"[Task {Task.CurrentId}]:\tI was able to run for {sw.ElapsedMilliseconds} milliseconds."+
+                                      $"The global time is {global.ElapsedMilliseconds} milliseconds");
+                    return;
+                }
+                else if (c == ch) 
+                {
+                    Console.WriteLine($"[Task {Task.CurrentId}]:\tFound {ch} at index {index}.");
+                    sw.Stop();
+                    Console.WriteLine($"[Task {Task.CurrentId}]:\tI found this in {sw.ElapsedMilliseconds} milliseconds. " +
+                                      $"The global time is {global.ElapsedMilliseconds} milliseconds");
+                    cts.Cancel();
+                    return;
+                }
+                Console.WriteLine($"[Task {Task.CurrentId}]:\tSearching for {ch}, I'm at index {index} and the global time is {global.ElapsedMilliseconds} milliseconds.");
+                index++;
+            }
         }
     }
 }
